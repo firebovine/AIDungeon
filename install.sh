257924eb95727ee8eb9c6c2c708ca0e6ba326d15
firebovine <firebovine@sorrynotsorry.com>
2019-12-07 22:47:09 -0500

    oops

--- install.sh
+++ install.sh
@@ -3,9 +3,7 @@ MODEL_VERSION=model_v5
 MODEL_NAME=model-550
 DOWNLOAD_URL=http://130.211.31.182:80
 
-cd ${MODELS_DIRECTORY}
-mkdir ${MODEL_VERSION}
-cd ${MODEL_VERSION}
+cd ${MODELS_DIRECTORY}/${MODEL_VERSION}
 apt-get install aria2 > /dev/null
 aria2c -x 16 -s 32 "${DOWNLOAD_URL}/${MODEL_VERSION}/${MODEL_NAME}.data-00000-of-00001"
 echo "Download Complete!"
